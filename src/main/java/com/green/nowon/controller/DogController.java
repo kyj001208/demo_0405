664d3988308a32ca4f71fd6f7dfe5ea6a6272028
Administrator <Administrator@304-19>
2024-04-05 10:44:14 +0900

    "추가 내용_스트링부트 사용하여 홈페이지 만들기"

--- src/main/java/com/green/nowon/controller/DogController.java
+++ src/main/java/com/green/nowon/controller/DogController.java
@@ -1,28 +1,29 @@
 package com.green.nowon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.green.nowon.domain.dto.Dog;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
 public class DogController {
 
+	
 	@GetMapping("/dog")
-	public String dog() {
+	public String list() { // 페이지 경로
 
-		return "views/list";
+		return "views/list"; // 템플릿 안에 있는 뷰 안에 리스트(html)로 들어가랏
 	}
 
 	@PostMapping("/dog")
-	public String save(
-			@RequestParam("name") String name, 
-			@RequestParam("breed")String breed, 
-			@RequestParam("age") int age) {
+	public String save(Dog dog) {// 객체 변수를 통해 간결화:Dog라는 클래스를 만들어 필드 및 세터 만들어주기
+		System.out.println(dog);
 
-		System.out.println("name: "+name);
-		System.out.println("breed: "+breed);
-		System.out.println("age: "+age);
 		return "redirect:/dog";
 
 	}
---
+++ src/main/java/com/green/nowon/domain/dto/Dog.java
@@ -0,0 +1,16 @@
+package com.green.nowon.domain.dto;
+
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter   
+public class Dog {
+	
+	private String name;
+	private String breed;
+	private int age;
+	
+
+	
+}
